2b536fe6d7b9fda72eeb4116ef43827b2d3e7334
RonaldyG1 <ronaldygalmeida@gmail.com>
2025-01-29 05:40:44 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,20 @@ const btnvoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoatual = 0;
 
+cartoes.forEach(cartao => {
+    cartao.addEventListener("click", function () {
+        const cartavirada = cartao.querySelector(".carta-virada");
+ 
+        // virar o cartão
+        cartao.classList.toggle("virar");
+        // mostrar o fundo da carta
+        cartavirada.classList.toggle("mostar-fundo-carta");
+    });
+});
+
 btnavancar.addEventListener("click", function () {
     if (cartaoatual === cartoes.length - 1) return;
-    
+
     escondercartaoselecionado();
 
     cartaoatual++;
